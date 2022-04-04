9b86d118fdb304249927fa76b32668bc1f25522b
Kevin McPeak <kevmc46@gmail.com>
2022-04-04 13:33:08 -0400

    may not need this

--- src/dom.js
+++ src/dom.js
@@ -1,65 +1,58 @@
-import { gameboards} from "./gameboard";
+import { gameboards } from "./gameboard";
 import { player } from "./player";
 
 let dom = (playerBoard) => {
-    let userBoard = document.querySelector(`#${playerBoard.boardName}-board`);
+	let userBoard = document.querySelector(`#${playerBoard.boardName}-board`);
 
-    let ships = () => {
-        playerBoard.shipTypes.forEach(ship => {
+	let ships = () => {
+		playerBoard.shipTypes.forEach((ship) => {
+			for (let i = 0; i < ship.len; i++) {
+				let block = document.createElement("div");
+				block.id = `${ship.name}-${i}`;
+				block.classList.add("shipBlocks");
+				document.getElementById(ship.name).appendChild(block);
+			}
+		});
+	};
 
-            for(let i = 0; i < ship.len; i++){
-                let block = document.createElement('div');    
-                block.id = `${ship.name}-${i}`;
-                block.classList.add('shipBlocks')
-                document.getElementById(ship.name).appendChild(block);
-            }
-        })
+	let renderBoard = () => {
+		clearBoard();
+		let count = 0;
+		playerBoard.board.forEach((cell) => {
+			cell.forEach((item) => {
+				let block = document.createElement("div");
+				block.classList.add("block");
+				if (count < 10) {
+					block.dataset.id = count.toString().padStart(2, "0");
+					count += 1;
+				} else {
+					block.dataset.id = count.toString();
+					count += 1;
+				}
 
-        
-    }
+				if (Number.isInteger(item)) {
+					block.classList.add("ship");
+				}
 
-    let renderBoard = () => {
-        clearBoard();
-        let count = 0;
-        playerBoard.board.forEach(cell => {
-            cell.forEach(item => {
-                let block = document.createElement('div');
-                block.classList.add("block");
-                if(count < 10){
-                    block.dataset.id = count.toString().padStart(2, "0");
-                    count +=1;
-                } else{
-                    block.dataset.id = count.toString();
-                    count += 1;
-                }
+				if (item === "X") {
+					block.classList.add("hit");
+				}
+				if (item === "O") {
+					block.classList.add("miss");
+				}
 
-                    if (Number.isInteger(item)){
-                        block.classList.add("ship")
-                    }
+				userBoard.appendChild(block);
+			});
+		});
+	};
 
-                    if (item === "X"){
-                        block.classList.add("hit");
-                    }
-                    if (item === "O"){
-                        block.classList.add("miss")
-                    }
-                
-                userBoard.appendChild(block);
-                   
-            })
-        })
-    }
+	let clearBoard = () => {
+		while (userBoard.firstChild) {
+			userBoard.removeChild(userBoard.firstChild);
+		}
+	};
 
-    let clearBoard = () => {
-
-        while (userBoard.firstChild) {
-            userBoard.removeChild(userBoard.firstChild)
-        }
-    
-    }
-
-   return {renderBoard, clearBoard}
-
+	return { renderBoard, clearBoard, ships };
 };
 
-export {dom}
+export { dom };
--- src/index.js
+++ src/index.js
@@ -1,37 +1,30 @@
-import './style.css';
+import "./style.css";
 let newShip = require("./shipConstructor");
-import {gameboards} from "./gameboard"
-import { dom } from './dom';
-import { gameController } from './game';
+import { gameboards } from "./gameboard";
+import { dom } from "./dom";
+import { gameController } from "./game";
 
+let dragShips = () => {
+	let draggableShips = document.querySelectorAll(".ship-start");
+	let dropZone = document.querySelectorAll(".game-cells");
 
+	draggableShips.forEach((ship) => {
+		ship.addEventListener("dragstart", () => {
+			ship.classList.add("dragging");
+		});
 
-// let dragShips = () => {
-    
-//     let draggableShips = document.querySelectorAll(".ship-start");
-//     let dropZone = document.querySelectorAll('.game-cells');
+		ship.addEventListener("dragend", () => {
+			ship.classList.remove("dragging");
+		});
+	});
 
-//     draggableShips.forEach(ship => {
-//         ship.addEventListener('dragstart', () => {
-//             ship.classList.add("dragging")
-//         })
-
-//         ship.addEventListener('dragend', () => {
-//             ship.classList.remove("dragging")
-//         })
-//     })
-
-//     dropZone.forEach(zone => {
-//         zone.addEventListener('dragover', (e) => {
-//             e.preventDefault();
-//             let draggable = document.querySelector(".dragging")
-//             zone.append(draggable);
-//         })
-//     })
-    
-
-    
-// }
-
-// dragShips();
-
+	dropZone.forEach((zone) => {
+		zone.addEventListener("dragover", (e) => {
+			e.preventDefault();
+			let draggable = document.querySelector(".dragging");
+			zone.append(draggable);
+		});
+	});
+};
+dom.ships();
+dragShips();
