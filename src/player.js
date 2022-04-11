9a01dbca3194ea5f5c8b3b8e552681992e5243fe
Kevin McPeak <kevmc46@gmail.com>
2022-04-11 16:03:56 -0400

    error message is space is taken

--- src/player.js
+++ src/player.js
@@ -3,7 +3,13 @@ import { gameboards } from "./gameboard";
 let player = (name, enemyBoard) => {
 	let attack = (x, y) => {
 		if (enemyBoard.board[x][y] === "X" || enemyBoard.board[x][y] === "O") {
-			console.log("this space is already taken");
+			document.getElementById("error").textContent =
+				"space is already taken";
+			setTimeout(() => {
+				document.getElementById("error").textContent = " ";
+			}, 1250);
+
+			attack();
 		} else {
 			enemyBoard.receieveAttack(x, y);
 		}
